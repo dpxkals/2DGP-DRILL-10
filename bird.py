a490dd48a86725543db0d3ebc106ce01d35f8e6a
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-11-04 20:57:45 +0900

    프레임을 14로 해서 문제였음 이것을 고칠 예정

--- bird.py
+++ bird.py
@@ -8,9 +8,9 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-TIME_PER_ACTION = 0.2 # 비둘기를 기준으로 약 0.2초라 함
+TIME_PER_ACTION = 0.8 # 비둘기를 기준으로 약 0.2초라 함
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 5
+FRAMES_PER_ACTION = 14
 FRAMES_PER_SECOND = FRAMES_PER_ACTION * ACTION_PER_TIME
 
 class Bird:
@@ -20,17 +20,20 @@ class Bird:
             Bird.image = load_image('bird_animation.png')
         self.x, self.y, self.dir = x, y, dir
         self.frame = 0
-        self.frame_y = 2
+        self.frameCnt = 5
+        self.frame_y = 338
     def draw(self):
         if self.dir == 1:
-            self.image.clip_draw(int(self.frame)* 183, self.frame_y * 169, 184, 169, self.x, self.y, 50, 45)
+            self.image.clip_draw(int(self.frame)* 183, self.frame_y, 183, 169, self.x, self.y, 50, 45)
         else :
-            self.image.clip_composite_draw(int(self.frame)* 183, 338, 184, 169, 0, 'v', self.x, self.y, 50, 45)
+            self.image.clip_composite_draw(int(self.frame)* 183, self.frame_y, 183, 169, 0, 'v', self.x, self.y, 50, 45)
         pass
 
     def update(self):
         if int(self.frame) == 4:
-            self.frame_y -= 1
-            if self.frame_y < 0 :
-                self.frame_y = 2
-        self.frame = (self.frame + FRAMES_PER_SECOND * game_framework.frame_time) % 5
+            self.frame_y = 169
+        elif int(self.frame) == 9:
+            self.frame_y = 0
+        elif int(self.frame) == 13:
+            self.frame_y = 338
+        self.frame = (self.frame + FRAMES_PER_SECOND * game_framework.frame_time) % 14
