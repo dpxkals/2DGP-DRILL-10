bc830043a991770bc7b08b8522bdcc80265389b8
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-11-04 21:01:31 +0900

    조금 복잡해졌지만 가로줄과 세로줄을 구별하고 프레임 타임은 스프라이트 이미지 개수로 해서 속도 조절

--- bird.py
+++ bird.py
@@ -21,19 +21,24 @@ class Bird:
         self.x, self.y, self.dir = x, y, dir
         self.frame = 0
         self.frameCnt = 5
+        self.current_frame = 0
         self.frame_y = 338
     def draw(self):
         if self.dir == 1:
-            self.image.clip_draw(int(self.frame)* 183, self.frame_y, 183, 169, self.x, self.y, 50, 45)
+            self.image.clip_draw(int(self.current_frame)* 183, self.frame_y, 183, 169, self.x, self.y, 50, 45)
         else :
-            self.image.clip_composite_draw(int(self.frame)* 183, self.frame_y, 183, 169, 0, 'v', self.x, self.y, 50, 45)
+            self.image.clip_composite_draw(int(self.current_frame)* 183, self.frame_y, 183, 169, 0, 'v', self.x, self.y, 50, 45)
         pass
 
     def update(self):
         if int(self.frame) == 4:
             self.frame_y = 169
+            self.frameCnt = 5
         elif int(self.frame) == 9:
             self.frame_y = 0
+            self.frameCnt = 4
         elif int(self.frame) == 13:
             self.frame_y = 338
+            self.frameCnt = 5
         self.frame = (self.frame + FRAMES_PER_SECOND * game_framework.frame_time) % 14
+        self.current_frame = (self.current_frame + FRAMES_PER_SECOND * game_framework.frame_time) % self.frameCnt
