23c52a7d1b6fa124970e8d40c04842f84ee68aae
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-11-04 20:01:06 +0900

    bird 클래스 x,y,dir 값 추가

--- bird.py
+++ bird.py
@@ -4,11 +4,10 @@ import game_framework
 
 class Bird:
     image = None
-    def __init__(self):
+    def __init__(self, x, y, dir):
         if Bird.image == None:
             Bird.image = load_image('bird_animation.png')
-        self.x, self.y
-
+        self.x, self.y, self.dir = x, y, dir
     def draw(self):
         self.image.draw(self.x, self.y)
         pass
