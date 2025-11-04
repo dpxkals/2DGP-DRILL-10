ef98354b316b23d32257c188f0d92ca7553d7962
민준홍 <120389042+dpxkals@users.noreply.github.com>
2025-11-04 19:55:18 +0900

    bird 기본 클래스 생성

---
+++ bird.py
@@ -0,0 +1,13 @@
+from pico2d import *
+import game_world
+import game_framework
+
+class Bird:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
